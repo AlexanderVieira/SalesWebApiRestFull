1dd15dff8057d56fe8368cebe3348163bcc94958
Alexander <Alexander@DESKTOP-8RM83T5>
2019-01-31 09:30:10 -0200

    Starting Layered Architecture Implementation.

---
+++ Sales.Infra.Data/Context/SalesCtx.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sales.Infra.Data.Context
+{
+    public class SalesCtx : DbContext
+    {
+        public SalesCtx()
+        {
+
+        }
+    }
+}
